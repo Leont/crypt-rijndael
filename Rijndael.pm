e5d62e49760810c7b4d7bef947f2c85f11637146
Leon Timmermans <fawaka@gmail.com>
2015-05-22 16:04:36 +0200

    Updated maintainer in docs, and reordered docs/code

--- Rijndael.pm
+++ Rijndael.pm
@@ -1,3 +1,18 @@
+package Crypt::Rijndael;
+
+use strict;
+use warnings;
+
+our $VERSION = '1.12';
+
+use XSLoader;
+
+XSLoader::load('Crypt::Rijndael', $VERSION);
+
+1;
+
+__END__
+
 =head1 NAME
 
 Crypt::Rijndael - Crypt::CBC compliant Rijndael encryption module
@@ -22,22 +37,6 @@ This module implements the Rijndael cipher, which has just been selected
 as the Advanced Encryption Standard.
 
 =over 4
-
-=cut
-
-package Crypt::Rijndael;
-use strict;
-use vars qw( $VERSION @ISA );
-
-use warnings;
-no warnings;
-
-require DynaLoader;
-
-$VERSION = '1.12';
-@ISA = qw/DynaLoader/;
-
-bootstrap Crypt::Rijndael $VERSION;
 
 =item keysize
 
@@ -111,7 +110,9 @@ Should EXPORT or EXPORT_OK the MODE constants.
 
 =head1 AUTHOR
 
-Currently maintained by brian d foy, C<< <bdfoy@cpan.org> >>.
+Currently maintained by Leon Timmermans C<< leont@cpan.org >>.
+
+Previously maintained by brian d foy, C<< <bdfoy@cpan.org> >>.
 
 Original code by  Rafael R. Sevilla.
 
@@ -122,7 +123,7 @@ and has been selected as the US Government's Advanced Encryption Standard.
 
 This code is in Github:
 
-	git://github.com/briandfoy/crypt-rijndael.git
+	git://github.com/leont/crypt-rijndael.git
 	
 =head1 LICENSE
 
@@ -130,6 +131,4 @@ This software is licensed under the Lesser GNU Public License v3 (29
 June 2007). See the included COPYING file for details.
 
 =cut
-
-1;
 
